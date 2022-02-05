de975d8015e6a058e9211dd1ad4fc349beed07a8
colin jaworski <colinrjaworski.84@gmail.com>
2022-02-05 13:26:44 -0600

    click listners added to all buttons and buttons log

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,55 @@
 $(document).ready(onReady);
+// Let firstButton = 
+
 
 function onReady() {
-    console.log('jquery is running', addInput());
+    $('#add-button').on('click', addClick);
+    $('#subtract-button').on('click', subtractClick);
+    $('#multiply-button').on('click', multiplyClick);
+    $('#divide-button').on('click', divideClick);
+    $('#equal-button').on('click', equalClick);
+    $('#clear-button').on('click', clearClick);
 }
 
-function addInput(){
-    $('#output').append('<h1>holla at ya boy!</h1>')
+
+function addClick(){
+    console.log('add button!')
+}
+function subtractClick(){
+    console.log('subtract button!')
+}
+function multiplyClick(){
+    console.log('multiply button!')
+}
+function divideClick(){
+    console.log('divide button!')
+}
+function equalClick(){
+    console.log('equal button!')
+}
+function clearClick(){
+    console.log('clear button!')
 }
 
+
+
+
+
+
+
+// function addItem() { 
+//     $.ajax({
+//       method: 'POST', 
+//       url: '/equation',  
+//       data: { 
+//               inputOne: $('#num-one').val(),
+//               inputTwo: $('#num-two').val(),
+//               operator: // this is where the button that was pressed will be assigned. 
+//       }
+//     }).then(function(response){
+        
+
+//     }).catch(function(response){
+//         console.log('UUUUUHHHGGG', response);
+//     })
+// }
